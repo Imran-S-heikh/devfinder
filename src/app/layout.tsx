e36461ad3467e8d72d9bfb6eff9ef99473abf08a
Imran-s-heikh <imransheikhsadi@gmail.com>
2024-08-11 12:05:46 +0600

    feat: Font Changed To Space Mono

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Space_Mono } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Space_Mono({ weight: ["400", "700"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title:
