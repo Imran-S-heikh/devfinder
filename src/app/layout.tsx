47a49f2ebddc22e1adb659954126b2ee420a70ea
Imran-s-heikh <imransheikhsadi@gmail.com>
2024-10-21 08:29:44 +0600

    feat: Metadata and OpenGraph Image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,45 @@ const inter = Space_Mono({ weight: ["400", "700"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title:
-    "GitHub User Search App - Find and Explore GitHub Profiles Effortlessly | Devfinder by Imran Shaikh",
+    "Devfinder - GitHub User Search App, Find and Explore GitHub Profiles Effortlessly" ,
   description:
     "Discover GitHub profiles with ease using our intuitive user search app. Simply enter a username to view detailed information, including repositories, followers, and more. Enjoy a seamless experience with light and dark mode options, responsive design for any device, and automatic theme detection based on your system preferences. Perfect for developers and GitHub enthusiasts alike",
+  keywords: [
+    "imran shaikh",
+    "javascript developer",
+    "solana developer",
+    "full stack developer",
+    "web developer",
+    "devfinder",
+    "frontend developer",
+    "react",
+    "nextjs",
+    "typescript",
+    "blockchain",
+    "rust",
+  ],
+  creator: "Imran Shaikh",
+  authors: [{ name: "Imran Shaikh" }],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    countryName: "Bangladesh",
+    description:
+      "Discover GitHub profiles with ease using our intuitive user search app. Simply enter a username to view detailed information, including repositories, followers, and more. Enjoy a seamless experience with light and dark mode options, responsive design for any device, and automatic theme detection based on your system preferences. Perfect for developers and GitHub enthusiasts alike",
+    title: "Devfinder",
+    alternateLocale: "bn_BD",
+    siteName:
+      "GitHub User Search App - Find and Explore GitHub Profiles Effortlessly | Devfinder by Imran Shaikh",
+    images: ["/og.png"],
+  },
+  twitter: {
+    card: "summary_large_image",
+    creator: "@Imran_S_haikh",
+    site: "@Imran_S_haikh",
+    title: "Devfinder - GitHub User Search App",
+    description:
+      "Discover GitHub profiles with ease using our intuitive user search app. Simply enter a username to view detailed information, including repositories, followers, and more. Enjoy a seamless experience with light and dark mode options, responsive design for any device, and automatic theme detection based on your system preferences. Perfect for developers and GitHub enthusiasts alike",
+  },
 };
 
 export default function RootLayout({
